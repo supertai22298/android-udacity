3f8da6517f3e00be8e489e4186afa71fdc10ee5d
Tai (Taffy) V. NGUYEN <taffy@enclave.vn>
2020-05-13 10:49:47 +0700

    RecyclerView, ListAdapter, BindingAdapter

--- andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -40,10 +40,10 @@ class OverviewViewModel : ViewModel() {
     val status: LiveData<String>
         get() = _status
 
-    private val _property = MutableLiveData<MarsProperty>()
+    private val _properties = MutableLiveData<List<MarsProperty>>()
 
-    val property: LiveData<MarsProperty>
-        get() = _property
+    val properties: LiveData<List<MarsProperty>>
+        get() = _properties
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
@@ -60,9 +60,9 @@ class OverviewViewModel : ViewModel() {
         coroutineScope.launch {
             var getPropertiesDeferred = MarsApi.retrofitService.getProperties()
             try {
-                var listResults = getPropertiesDeferred.await()
-                if (listResults?.size > 0){
-                    _property.value = listResults[0]
+                var listResult = getPropertiesDeferred.await()
+                if (listResult?.size > 0){
+                    _properties.value = listResult
                 }
             }catch (e: Exception) {
                 _status.value = "Failure ${e.message}"
