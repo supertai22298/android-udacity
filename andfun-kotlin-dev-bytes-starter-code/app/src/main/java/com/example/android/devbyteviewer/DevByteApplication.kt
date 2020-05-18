e890128b45a4c0d2044b393f716f5c26c925e3bb
Tai (Taffy) V. NGUYEN <taffy@enclave.vn>
2020-05-18 09:29:05 +0700

    Add constraint work manager

--- andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -18,9 +18,8 @@
 package com.example.android.devbyteviewer
 
 import android.app.Application
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
+import android.os.Build
+import androidx.work.*
 import com.example.android.devbyteviewer.work.RefreshDataWork
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -53,7 +52,19 @@ class DevByteApplication : Application() {
     }
 
     private fun setupRecurringWork() {
+        val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.METERED)
+                .setRequiresBatteryNotLow(true)
+                .setRequiresCharging(true)
+                .apply {
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        setRequiresDeviceIdle(true)
+                    }
+                }
+                .build()
+
         val repeatRequest = PeriodicWorkRequestBuilder<RefreshDataWork>(1, TimeUnit.DAYS)
+                .setConstraints(constraints)
                 .build()
         WorkManager.getInstance().enqueueUniquePeriodicWork(
                 RefreshDataWork.WORK_NAME,
