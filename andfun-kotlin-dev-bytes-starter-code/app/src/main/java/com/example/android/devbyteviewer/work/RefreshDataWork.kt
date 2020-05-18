b20e2a520e0133e6f6787f667a986d0db9e63c00
Tai (Taffy) V. NGUYEN <taffy@enclave.vn>
2020-05-18 09:06:36 +0700

    Create a worker

--- andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
+++ andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -16,3 +16,25 @@
  */
 
 package com.example.android.devbyteviewer.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
+import java.lang.Exception
+
+class RefreshDataWork(appContext: Context, params: WorkerParameters) : CoroutineWorker(appContext, params) {
+
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (e: Exception) {
+            Result.failure()
+        }
+    }
+}
